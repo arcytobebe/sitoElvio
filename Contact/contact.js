3fb34f1b1879b58e1633521d8b8760b187f6ca65
arcytobebe <arcy.infomusic@gmail.com>
2025-12-22 20:41:24 +0100

    Delete Contact directory

--- Contact/contact.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-hamburger.addEventListener('click', () => {
-    mobileMenu.classList.toggle('show');
-});
