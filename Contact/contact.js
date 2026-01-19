aa2700593bca2f559eb045a87960686e88b041ec
arcytobebe <arcy.infomusic@gmail.com>
2026-01-19 22:55:19 +0100

    Update contact.js

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,6 +1,22 @@
+// =========================
+// HAMBURGER MENU MOBILE
+// =========================
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('show');
 });
+
+// =========================
+// GESTIONE FORM
+// =========================
+const form = document.getElementById('contactForm');
+const formMessage = document.getElementById('formMessage');
+
+form.addEventListener('submit', function(e) {
+    formMessage.style.display = 'block';
+    formMessage.style.color = 'green';
+    formMessage.textContent = "Messaggio inviato! Grazie per avermi contattato.";
+});
+
